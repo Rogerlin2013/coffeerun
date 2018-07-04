3ca4e38ec84ceb139690e5aad968ab841f92a164
linyongzhi <yongzhi.lin@uama.com.cn>
2018-07-04 16:01:43 +0800

    增加对于form提交的处理逻辑

---
+++ scripts/formhandler.js
@@ -0,0 +1,41 @@
+(function (window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+
+        this.$formElement = $(selector);
+        if (this.$formElement.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+
+        this.$strengthLevel = $("#strengthLevel");
+        this.$strengthLevel.on('change', function (event) {
+            $("#strengthLevelLabel").html('Current strength: ' + event.currentTarget.value);
+        });
+    }
+
+    FormHandler.prototype.addSubmitHandler = function (fn) {
+        console.log('Setting submit handler for form');
+        this.$formElement.on('submit', function (event) {
+            event.preventDefault();
+
+            var data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            fn(data);
+            this.reset();
+            this.elements[0].focus();
+            console.log(data);
+        });
+    };
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+})(window);
